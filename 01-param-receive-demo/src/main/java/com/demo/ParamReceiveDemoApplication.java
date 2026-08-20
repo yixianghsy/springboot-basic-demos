398eb442e2f355c99afdc425ea47647c7c293083
yixianghsy <yixianghsy@gmail.com>
2026-08-21 00:42:37 +0800

    springboot 基础 请求参数接收

--- 01-param-receive-demo/src/main/java/com/demo/ParamReceiveDemoApplication.java
+++ 01-param-receive-demo/src/main/java/com/demo/ParamReceiveDemoApplication.java
@@ -2,7 +2,7 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//请求参数接收
 @SpringBootApplication
 public class ParamReceiveDemoApplication {
     public static void main(String[] args) {
