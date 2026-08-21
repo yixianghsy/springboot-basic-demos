ab46827f3eb465241c4819b026c9db1135f6cd49
yixianghsy <yixianghsy@gmail.com>
2026-08-21 15:46:26 +0800

    springboot 基础 多环境配置切换

--- 10-profile-env-demo/src/main/java/com/demo/ProfileEnvDemoApplication.java
+++ 10-profile-env-demo/src/main/java/com/demo/ProfileEnvDemoApplication.java
@@ -1,11 +1,19 @@
 package com.demo;
-
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfileEnvDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProfileEnvDemoApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(ProfileEnvDemoApplication.class, args);
+        MutablePropertySources sources = ctx.getEnvironment().getPropertySources();
+        System.out.println("====打印所有已加载配置源====");
+        for (PropertySource<?> ps : sources) {
+            System.out.println(ps.getName());
+        }
     }
+
 }
